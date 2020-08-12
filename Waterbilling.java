a37dad3fe67fcd337deadc5499a48b51f16ff0a8
kshitiz-kothari <kshitiz.kothari@cs.christuniversity.in>
2020-08-12 21:52:16 +0530

    program structure 0

--- Waterbilling.java
+++ Waterbilling.java
@@ -4,23 +4,37 @@ import java.io.File;
 
 class Waterbilling
 {
+	static String location;
+	static String month;
 	public static void confirm_print_loc()
 	{
 		Scanner in=new Scanner(System.in);
 		System.out.println("Enter the location to be printed : ");
-		String location=in.nextLine();
-		
+		location=in.nextLine();
 	}
-
+	public static void confirm_month()
+	{
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the location to be printed : ");
+		month=in.nextLine();
+	}
+	public void dm(int cons){}
+	public void nd(int cons){}
+	public void in(int cons){}
 	public static void main(String args[])
 	{
 		Scanner in=new Scanner(System.in);
-		System.out.println("Enter the file name : ");
+		System.out.println("Enter the source file name : ");
 		String file_name = in.nextLine();
 		File input_file = new File (file_name);
 		confirm_print_loc();
-		//FileInputStream fis=new FileInputStream(new File("E:\\java\\waterdata.xls")); 
-
+		confirm_month();
+		int rates[];
+		int meter_rent=40;
+		int[] fixed_charges={50,100,200};
+		int[] min={100,200,300};
+		String[] conn_type={"DM","ND","IN"};
+		System.out.println(location);
 		if (input_file.exists())
 		 {
 			System.out.println(input_file.getName() + " exists");
@@ -35,6 +49,7 @@ class Waterbilling
 				System.exit(0);
 			}
 		}
+		
 	}
 	
 }
